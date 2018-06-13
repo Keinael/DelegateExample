72586d665d42608025cd31b016c436da06c5825b
DevBidon <dev@bidon-gs.com>
2018-06-13 10:26:09 +0300

    Refactor. Change AsyncDelegate1 script logic.

--- Assets/Scripts/AsyncDelegate1.cs
+++ Assets/Scripts/AsyncDelegate1.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using UnityEngine;
 
 public delegate int SampleDelegate(string data);
 
 public class AsyncDelegate1 : MonoBehaviour
 {
-    void Start()
+    private void Start()
     {
         SampleDelegate counter = CountCharacters;
         SampleDelegate parser = Parse;
 
-        IAsyncResult counterResult = counter.BeginInvoke("hello", null, null);
-        IAsyncResult parserResult = parser.BeginInvoke("10", null, null);
+        var counterResult = counter.BeginInvoke("hello", null, null);
+        var parserResult = parser.BeginInvoke("10", null, null);
 
         print("Основной поток с ID = " + Thread.CurrentThread.ManagedThreadId + " продолжает выполняться");
 
@@ -24,15 +23,27 @@ public class AsyncDelegate1 : MonoBehaviour
 
     private static int CountCharacters(string text)
     {
-        Thread.Sleep(5000);
-        print("Подсчет символов в строке " + text + " в потоке с ID = " + Thread.CurrentThread.ManagedThreadId);
-        return text.Length;
+//        Thread.Sleep(5000);
+//        print("Подсчет символов в строке " + text + " в потоке с ID = " + Thread.CurrentThread.ManagedThreadId);
+        for (var i = 0; i < 100; i += 10)
+        {
+            print("Поток: " + Thread.CurrentThread.ManagedThreadId + ". I = " + i);
+        }
+
+//        return text.Length;
+        return 0;
     }
 
     private static int Parse(string text)
     {
-        Thread.Sleep(100);
-        print("Парсинг строки " + text + " в потоке с ID = " + Thread.CurrentThread.ManagedThreadId);
-        return int.Parse(text);
+//        Thread.Sleep(100);
+//        print("Парсинг строки " + text + " в потоке с ID = " + Thread.CurrentThread.ManagedThreadId);
+        for (var i = 0; i < 100; i += 10)
+        {
+            print("Поток: " + Thread.CurrentThread.ManagedThreadId + ". J = " + i);
+        }
+
+//        return int.Parse(text);
+        return 1;
     }
 }
--- Assets/Scripts/DictionaryLambda.cs
+++ Assets/Scripts/DictionaryLambda.cs
@@ -1,37 +1,40 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DictionaryLambda : MonoBehaviour
 {
-    public string operation;
+    public string Operation;
     public int X;
     public int Y;
-    
-    public void Start()
+
+    private void Start()
     {
-        print(Call(operation));
+        print(Call(Operation));
     }
 
     private int Call(string message)
     {
-        if (message != null)
+        if (message == null)
         {
-            if (!_dSelector.ContainsKey(message)) return _dSelector["default"](0, 0);
+           print("Message is null");
+        }
+        else if (!_dSelector.ContainsKey(message))
+        {
+            return _dSelector["default"](0, 0);
         }
         
         return _dSelector[message](X, Y);
     }
-    
+
     private static Dictionary<string, Func<int, int, int>> _dSelector = new Dictionary<string, Func<int, int, int>>
     {
         {"reset", (x, y) => 0},
         {"substraction", (x, y) => x - y},
         {"addition", (x, y) => x + y},
         {"multiplication", (x, y) => x * y},
-        {"xx", (x, y) => ((x/100) * y)},
-        { "1k", (x, y) => 1000},
+        {"xx", (x, y) => ((x / 100) * y)},
+        {"1k", (x, y) => 1000},
         {"default", (x, y) => x},
     };
 }
